eda862b42cf63fc42f279fcc05ddb159222df041
Di <di008@csiro.au>
2020-03-12 12:24:50 +1100

    small modification of the demo app

--- plotly-shiny-demo/app.R
+++ plotly-shiny-demo/app.R
@@ -3,27 +3,22 @@ library(plotly)
 
 ui <- fluidPage(
   selectizeInput(
-    inputId = "cities",
-    label = "select a city",
-    choices = unique(txhousing$city),
+    inputId = "cities", 
+    label = "Select a city", 
+    choices = unique(txhousing$city), 
     selected = "Abilene",
     multiple = TRUE
   ),
-  plotOutput(outputId = "p")
+  plotlyOutput(outputId = "p")
 )
 
-server <- function(input, output) {
-  
+server <- function(input, output, ...) {
   output$p <- renderPlotly({
-    
     plot_ly(txhousing, x = ~date, y = ~median) %>%
-        filter(city %in% input$cities) %>%
-        group_by(city) %>%
-        add_lines()
-  
-    })
-  
+      filter(city %in% input$cities) %>%
+      group_by(city) %>%
+      add_lines()
+  })
 }
 
-
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
